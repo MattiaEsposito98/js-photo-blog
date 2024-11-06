c68d7a7b5caaed66853b4f95d21e20af9e1f128d
MattiaEsposito98 <esposito.matty@hotmail.it>
2024-11-06 15:36:58 +0100

    add axios in js

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-console.log ("ciao")
+const rowEl = document.querySelector('.row')
+console.log(rowEl)
+
+
+axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
+  .then((res) => {
+    console.log(res)
+    const cards = res.data
+    console.log(cards)
+
+    appendCards (cards,rowEl)
+  })
+
+  .catch((err) => {
+    console.log(err)
+  })
+
+
+function appendCards(cards, root) {
+  cards.forEach(card => {
+    const {url, title} = card
+
+    const cardsHTML = `
+  <div class="col-3">
+    <div class="card">
+      <img src="./img/pin.svg" class="decoration-card">
+      <img src=${url} alt="">
+      <p>${title}</p>
+    </div>
+  </div>
+    `
+    root.innerHTML += cardsHTML
+  });
+}
+
+
+
+
