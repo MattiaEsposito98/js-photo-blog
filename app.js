682b3a06fd478fc0c99c7236566782598da0858a
MattiaEsposito98 <esposito.matty@hotmail.it>
2024-11-07 16:04:17 +0100

    add overlay al click sulla card

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const wallEl = document.querySelector('.wall')
 console.log(wallEl)
+let overlay = document.querySelector('.container-cardBig')
+console.log (overlay)
 
 
 axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
@@ -8,7 +10,7 @@ axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
     const cards = res.data
     console.log(cards)
 
-    appendCards (cards,wallEl)
+    appendCards(cards, wallEl)
   })
 
   .catch((err) => {
@@ -18,7 +20,7 @@ axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
 
 function appendCards(cards, root) {
   cards.forEach(card => {
-    const {url, title} = card
+    const { url, title } = card
 
     const cardsHTML = `
   <div class="col-3">
@@ -33,27 +35,49 @@ function appendCards(cards, root) {
   });
 
 
- // Aggiungere l'evento click per ogni card
- const cardsGenerate = document.querySelectorAll('.card');
- cardsGenerate.forEach(card => {
-  card.addEventListener('click', function() {
-     card.classList.toggle('hovered');
-     const button = document.createElement ('button')
-     button.textContent = "Chiudi"
-     document.body.appendChild(button)
 
-    //  Funzione per rimuovere bottone
-    function closeCard () {
-      button.removeEventListener ('click', closeCard)
-      button.remove()
-      location.reload();
-    }
-     button.addEventListener ('click', closeCard)
-     
 
-     }, { once: true } );   //once: true per effettuare solo una volta l'evento
- });
+  // Aggiungere l'evento click per ogni card
+  const cardsGenerate = document.querySelectorAll('.card');
+  console.log(cardsGenerate)
+  cardsGenerate.forEach(card => {
+
+
+    // Creo elementi
+    card.addEventListener('click', function () {
+      card.classList.toggle('hovered');
+      const button = document.createElement('button')
+      button.textContent = "Chiudi"
+      document.body.appendChild(button)
+      let cardBig = document.createElement('img')
+      cardBig.style.width = '80%';
+      cardBig.style.height = '80%';
+      cardBig.style.position = 'absolute';
+      cardBig.style.top = '50%';
+      cardBig.style.left = '58%';
+      cardBig.style.transform = 'translate(-50%, -50%)';
+      cardBig.src = card.url
+
+
+
+      overlay.appendChild(cardBig)
+      overlay.style.display = 'flex'
+
+
+      //  Funzione per rimuovere bottone
+      function closeCard() {
+        button.remove()
+        location.reload();
+      }
+      button.addEventListener('click', closeCard)
+
+
+    },);   //once: true per effettuare solo una volta l'evento    { once: true }
+  });
+
+
 }
+
 
 
 
